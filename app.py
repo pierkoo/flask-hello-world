7635c7b540400650d2457bc2694bf0b6c9133d45
pierkoo <pierkoo@gmail.com>
2020-02-17 22:34:36 +0100

    second commit

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask import Flask
 # Create the application object
 app = Flask(__name__)
 
+# Error handling
+app.config["DEBUG"] = True
+
 # Use the decorator pattern to
 # link the view function t oa url
 @app.route("/")
@@ -13,7 +16,36 @@ app = Flask(__name__)
 # Define the view using a function, wchich returns a string
 
 def hello_world():
-    return "Hello World!"
+    return "Hello World!????"
 
+# Dynamic route
+@app.route("/test")
+@app.route("/test/<search_query>")
+
+def search(search_query="hi"):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return f"{value} + 1 = {value +1} correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return f"{value} + 1 = {value +1} correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return value
+
+# Dynamic route witch explicit statuscodes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return f"Hello {name}", 200
+    else:
+        return "Not Found", 404
 if __name__ == "__main__":
     app.run()
